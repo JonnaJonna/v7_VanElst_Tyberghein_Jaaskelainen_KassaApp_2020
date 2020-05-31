548c35e6b347fd3b6e8453804126f6610d957df0
Jonna Jääskeläinen <jonna.jaaskelainen@student.lamk.fi>
2020-05-31 13:37:38 +0200

    Added ticket options for kassa properties

--- src/model/KassaProperties.java
+++ src/model/KassaProperties.java
@@ -17,10 +17,19 @@ import java.util.Properties;
 public class KassaProperties {
     private File filename = new File("src/files/config.properties");
     private Properties properties = new Properties();
-
+    //Options for filetype
     private String loadSaveStrategy;
+    //Options for discount
     private String discountStrategy;
     private String discountParams;
+    //Options for ticket
+    private boolean needsHeaderText;
+    private boolean needsDateAndTime;
+    private boolean needsDiscountDetails;
+    private boolean needsVATDetails;
+    private boolean needsFooterText;
+    private String headerText;
+    private String footerText;
 
     public KassaProperties(){
         try(InputStream fis = new FileInputStream(filename)){
@@ -29,6 +38,13 @@ public class KassaProperties {
             this.loadSaveStrategy = properties.getProperty("loadSaveStrategy"); //create the correct strategy class
             this.discountStrategy = properties.getProperty("discountStrategy");
             this.discountParams = properties.getProperty("discountParams");
+            this.needsHeaderText = Boolean.parseBoolean(properties.getProperty("needsHeaderText"));
+            this.needsDateAndTime = Boolean.parseBoolean(properties.getProperty("needsDateAndTime"));
+            this.needsDiscountDetails = Boolean.parseBoolean(properties.getProperty("needsDiscountDetails"));
+            this.needsVATDetails = Boolean.parseBoolean(properties.getProperty("needsVATDetails"));
+            this.needsFooterText = Boolean.parseBoolean(properties.getProperty("needsFooterText"));
+            this.headerText = properties.getProperty("headerText");
+            this.footerText = properties.getProperty("footerText");
             fis.close();
         }catch (IOException e) {
             throw new RuntimeException(e);
@@ -58,6 +74,13 @@ public class KassaProperties {
             properties.setProperty("loadSaveStrategy", getLoadSaveStrategy());
             properties.setProperty("discountStrategy", getDiscountStrategy());
             properties.setProperty("discountParams", getDiscountParams());
+            properties.setProperty("needsHeaderText", String.valueOf(getNeedsHeaderText()));
+            properties.setProperty("needsDateAndTime", String.valueOf(getNeedsDateAndTime()));
+            properties.setProperty("needsDiscountDetails", String.valueOf(getNeedsDiscountDetails()));
+            properties.setProperty("needsVATDetails", String.valueOf(getNeedsVATDetails()));
+            properties.setProperty("needsFooterText", String.valueOf(getNeedsFooterText()));
+            properties.setProperty("headerText", getHeaderText());
+            properties.setProperty("footerText", getFooterText());
             properties.store(fos, null);
             fos.close();
         } catch (IOException e) {
@@ -88,4 +111,60 @@ public class KassaProperties {
     public void setDiscountParams(String discountParams) {
         this.discountParams = discountParams;
     }
+
+    public boolean getNeedsHeaderText() {
+        return needsHeaderText;
+    }
+
+    public boolean getNeedsDateAndTime() {
+        return needsDateAndTime;
+    }
+
+    public boolean getNeedsDiscountDetails() {
+        return needsDiscountDetails;
+    }
+
+    public boolean getNeedsVATDetails() {
+        return needsVATDetails;
+    }
+
+    public boolean getNeedsFooterText() {
+        return needsFooterText;
+    }
+
+    public String getHeaderText() {
+        return headerText;
+    }
+
+    public String getFooterText() {
+        return footerText;
+    }
+    public void setNeedsHeaderText(boolean needsHeaderText){
+        this.needsHeaderText = needsHeaderText;
+    }
+
+    public void setNeedsDateAndTime(boolean needsDateAndTime) {
+        this.needsDateAndTime = needsDateAndTime;
+    }
+
+    public void setNeedsDiscountDetails(boolean needsDiscountDetails) {
+        this.needsDiscountDetails = needsDiscountDetails;
+    }
+
+    public void setNeedsVATDetails(boolean needsVATDetails) {
+        this.needsVATDetails = needsVATDetails;
+    }
+
+    public void setNeedsFooterText(boolean needsFooterText) {
+        this.needsFooterText = needsFooterText;
+    }
+
+    public void setHeaderText(String headerText) {
+        this.headerText = headerText;
+    }
+
+    public void setFooterText(String footerText) {
+        this.footerText = footerText;
+    }
+
 }
